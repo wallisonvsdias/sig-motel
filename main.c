59af59e84a39b70a4c6522bff9978b4f7c67aaec
wallisonvsdias <wallison.dias.711@ufrn.edu.br>
2025-08-18 14:16:04 -0300

    feat: criação do programa principal

---
+++ main.c
@@ -0,0 +1,50 @@
+//⠀⠀⠀⠀⠀⠀                                               ⠀⣠⣶⣶⣶⣦                                                         
+//  ____ ___ ____       __  __       _       _ ⠀ ⠀⣠⣤⣤⣄⣀⣾⣿⠟⠛⠻⢿⣷
+// / ___|_ _/ ___|     |  \/  | ___ | |_ ___| | ⢰⣿⡿⠛⠙⠻⣿⣿⠁⠀⠀⠀⢸⣿⡇
+// \___ \| | |  _ _____| |\/| |/ _ \| __/ _ \ | ⢿⣿⣇⠀⠀⠀⠈⠏⠀⠀⠀⠀⣼⣿⠀
+//  ___) | | |_| |_____| |  | | (_) | ||  __/ | ⠀⠻⣿⣷⣦⣤⣀⠀⠀⠀⠀⣾⡿⠃⠀
+// |____/___\____|     |_|  |_|\___/ \__\___|_| ⠀⠀⠀⠀⠉⠉⠻⣿⣄⣴⣿⠟⠀⠀⠀
+//                                              ⠀⠀⠀⠀⠀⠀⠀⣿⡿⠟⠁⠀⠀⠀⠀
+//
+// Universidade Federal do Rio Grande do Norte - UFRN
+// Centro de Ensino Superior do Seridó - CERES
+// Departamento de Computação e Tecnologia - DCT
+// Disciplina DCT1106 -- Programação
+// Projeto Sistema de Gestão para Motéis
+// Desenvolvido por @wallisonvsdias e @Isabellecavalcant -- desde Ago, 2025
+
+
+#include <stdio.h>
+// #include <stdlib.h>
+
+
+// Assinatura das funções
+void tela_menu_principal(void);
+void tela_sobre(void);
+void tela_equipe(void);
+
+void tela_menu_cliente(void);
+void tela_menu_funcionario(void);
+void tela_menu_quarto(void);
+void tela_menu_produto(void);
+void tela_menu_hospedagem(void);
+void tela_menu_venda(void);
+void tela_menu_relatorio(void);
+
+
+// Programa principal
+int main(void) {
+    tela_menu_principal;
+    tela_sobre;
+    tela_equipe;
+
+    tela_menu_cliente;
+    tela_menu_funcionario;
+    tela_menu_quarto;
+    tela_menu_produto;
+    tela_menu_hospedagem;
+    tela_menu_venda;
+    tela_menu_relatorio;
+    
+    return 0;
+}
