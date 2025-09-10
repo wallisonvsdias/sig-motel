a4e707fc34b14a5ab38c2c24f4344cb316108754
wallisonvsdias <wallison.dias.711@ufrn.edu.br>
2025-09-10 15:51:23 -0300

    feat: modulariza o menu de produtos

--- main.c
+++ main.c
@@ -16,6 +16,7 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include "produtos.h"
 
 
 // Assinatura das funções
@@ -46,13 +47,6 @@ void cadastrar_hospedagem(void);
 void exibir_hospedagem(void);
 void alterar_hospedagem(void);
 void excluir_hospedagem(void);
-
-
-void menu_produto(void);
-void cadastrar_produto(void);
-void exibir_produto(void);
-void alterar_produto(void);
-void excluir_produto(void);
 
 
 void menu_venda(void);
@@ -775,151 +769,6 @@ void excluir_hospedagem(void) {
     printf("|                              Excluir Hospedagem                             |\n");
     printf("|                                                                             |\n");
     printf("|      Informe o ID da hospedagem:                                            |\n");
-    printf("|                                                                             |\n");
-    printf("♡--------˖⁺‧₊˚♡˚₊‧⁺˖--------------˖⁺‧₊˚♡˚₊‧⁺˖--------------˖⁺‧₊˚♡˚₊‧⁺˖--------♡\n");
-    printf("\n");
-    getchar();
-    printf("Pressione ENTER para voltar ao menu...");
-    getchar();
-}
-
-
-void menu_produto(void) {
-    char op_produto;
-    system("clear||cls");
-    printf("♡--------˖⁺‧₊˚♡˚₊‧⁺˖--------------˖⁺‧₊˚♡˚₊‧⁺˖--------------˖⁺‧₊˚♡˚₊‧⁺˖--------♡\n");
-    printf("|           Universidade Federal do Rio Grande do Norte - UFRN                |\n");
-    printf("|               Centro de Ensino Superior do Seridó - CERES                   |\n");
-    printf("|                     Disciplina DCT1106 -- Programação                       |\n");
-    printf("|                   Projeto Sistema de Gestão para Motéis                     |\n");
-    printf("|  Desenvolvido por @wallisonvsdias e @Isabellecavalcant -- desde Ago, 2025   |\n");
-    printf("|                                                                             |\n");
-    printf("|                                   SIG-Motel                                 |\n");
-    printf("♡--------˖⁺‧₊˚♡˚₊‧⁺˖--------------˖⁺‧₊˚♡˚₊‧⁺˖--------------˖⁺‧₊˚♡˚₊‧⁺˖--------♡\n");
-    printf("|                                                                             |\n");
-    printf("|                               Módulo Produto                                |\n");
-    printf("|                                                                             |\n");
-    printf("|      1 - Cadastrar Produto                                                  |\n");
-    printf("|      2 - Exibir Dados do Produto                                            |\n");
-    printf("|      3 - Alterar Dados do Produto                                           |\n");
-    printf("|      4 - Excluir Produto                                                    |\n");
-    printf("|      0 - Retornar ao Menu Principal                                         |\n");
-    printf("|                                                                             |\n");
-    printf("♡--------˖⁺‧₊˚♡˚₊‧⁺˖--------------˖⁺‧₊˚♡˚₊‧⁺˖--------------˖⁺‧₊˚♡˚₊‧⁺˖--------♡\n");
-    printf("\n");
-    printf("Escolha sua opção: ");
-    scanf(" %c", &op_produto);
-    getchar();
-    
-    switch(op_produto) {
-        case '1':
-            cadastrar_produto();
-            break;
-        case '2':
-            exibir_produto();
-            break;
-        case '3':
-            alterar_produto();
-            break;
-        case '4':
-            excluir_produto();
-            break;
-        default:
-            printf("Por favor, digite uma opção válida");
-            break;
-    }
-}
-
-
-void cadastrar_produto(void) {
-    system("clear||cls");
-    printf("♡--------˖⁺‧₊˚♡˚₊‧⁺˖--------------˖⁺‧₊˚♡˚₊‧⁺˖--------------˖⁺‧₊˚♡˚₊‧⁺˖--------♡\n");
-    printf("|           Universidade Federal do Rio Grande do Norte - UFRN                |\n");
-    printf("|               Centro de Ensino Superior do Seridó - CERES                   |\n");
-    printf("|                     Disciplina DCT1106 -- Programação                       |\n");
-    printf("|                   Projeto Sistema de Gestão para Motéis                     |\n");
-    printf("|  Desenvolvido por @wallisonvsdias e @Isabellecavalcant -- desde Ago, 2025   |\n");
-    printf("|                                                                             |\n");
-    printf("|                                   SIG-Motel                                 |\n");
-    printf("♡--------˖⁺‧₊˚♡˚₊‧⁺˖--------------˖⁺‧₊˚♡˚₊‧⁺˖--------------˖⁺‧₊˚♡˚₊‧⁺˖--------♡\n");
-    printf("|                                                                             |\n");
-    printf("|                            Cadastrar Produto                                |\n");
-    printf("|                                                                             |\n");
-    printf("|      Nome:                                                                  |\n");
-    printf("|      Valor:                                                                 |\n");
-    printf("|      Quantidade:                                                            |\n");
-    printf("|                                                                             |\n");
-    printf("♡--------˖⁺‧₊˚♡˚₊‧⁺˖--------------˖⁺‧₊˚♡˚₊‧⁺˖--------------˖⁺‧₊˚♡˚₊‧⁺˖--------♡\n");
-    printf("\n");
-    getchar();
-    printf("Pressione ENTER para voltar ao menu...");
-    getchar();
-}
-
-
-void exibir_produto(void){
-    system("clear||cls");
-    printf("♡--------˖⁺‧₊˚♡˚₊‧⁺˖--------------˖⁺‧₊˚♡˚₊‧⁺˖--------------˖⁺‧₊˚♡˚₊‧⁺˖--------♡\n");
-    printf("|           Universidade Federal do Rio Grande do Norte - UFRN                |\n");
-    printf("|               Centro de Ensino Superior do Seridó - CERES                   |\n");
-    printf("|                     Disciplina DCT1106 -- Programação                       |\n");
-    printf("|                   Projeto Sistema de Gestão para Motéis                     |\n");
-    printf("|  Desenvolvido por @wallisonvsdias e @Isabellecavalcant -- desde Ago, 2025   |\n");
-    printf("|                                                                             |\n");
-    printf("|                                   SIG-Motel                                 |\n");
-    printf("♡--------˖⁺‧₊˚♡˚₊‧⁺˖--------------˖⁺‧₊˚♡˚₊‧⁺˖--------------˖⁺‧₊˚♡˚₊‧⁺˖--------♡\n");
-    printf("|                                                                             |\n");
-    printf("|                           Exibir Dados do Produto                           |\n");
-    printf("|                                                                             |\n");
-    printf("|      Informe o ID do produto:                                               |\n");
-    printf("|                                                                             |\n");
-    printf("♡--------˖⁺‧₊˚♡˚₊‧⁺˖--------------˖⁺‧₊˚♡˚₊‧⁺˖--------------˖⁺‧₊˚♡˚₊‧⁺˖--------♡\n");
-    printf("\n");
-    getchar();
-    printf("Pressione ENTER para voltar ao menu...");
-    getchar();
-}
-
-
-void alterar_produto(void){
-    system("clear||cls");
-    printf("♡--------˖⁺‧₊˚♡˚₊‧⁺˖--------------˖⁺‧₊˚♡˚₊‧⁺˖--------------˖⁺‧₊˚♡˚₊‧⁺˖--------♡\n");
-    printf("|           Universidade Federal do Rio Grande do Norte - UFRN                |\n");
-    printf("|               Centro de Ensino Superior do Seridó - CERES                   |\n");
-    printf("|                     Disciplina DCT1106 -- Programação                       |\n");
-    printf("|                   Projeto Sistema de Gestão para Motéis                     |\n");
-    printf("|  Desenvolvido por @wallisonvsdias e @Isabellecavalcant -- desde Ago, 2025   |\n");
-    printf("|                                                                             |\n");
-    printf("|                                   SIG-Motel                                 |\n");
-    printf("♡--------˖⁺‧₊˚♡˚₊‧⁺˖--------------˖⁺‧₊˚♡˚₊‧⁺˖--------------˖⁺‧₊˚♡˚₊‧⁺˖--------♡\n");
-    printf("|                                                                             |\n");
-    printf("|                           Alterar Dados do Produto                          |\n");
-    printf("|                                                                             |\n");
-    printf("|      Informe o ID do produto:                                               |\n");
-    printf("|                                                                             |\n");
-    printf("♡--------˖⁺‧₊˚♡˚₊‧⁺˖--------------˖⁺‧₊˚♡˚₊‧⁺˖--------------˖⁺‧₊˚♡˚₊‧⁺˖--------♡\n");
-    printf("\n");
-    getchar();
-    printf("Pressione ENTER para voltar ao menu...");
-    getchar();
-}
-
-
-void excluir_produto(void){
-    system("clear||cls");
-    printf("♡--------˖⁺‧₊˚♡˚₊‧⁺˖--------------˖⁺‧₊˚♡˚₊‧⁺˖--------------˖⁺‧₊˚♡˚₊‧⁺˖--------♡\n");
-    printf("|           Universidade Federal do Rio Grande do Norte - UFRN                |\n");
-    printf("|               Centro de Ensino Superior do Seridó - CERES                   |\n");
-    printf("|                     Disciplina DCT1106 -- Programação                       |\n");
-    printf("|                   Projeto Sistema de Gestão para Motéis                     |\n");
-    printf("|  Desenvolvido por @wallisonvsdias e @Isabellecavalcant -- desde Ago, 2025   |\n");
-    printf("|                                                                             |\n");
-    printf("|                                   SIG-Motel                                 |\n");
-    printf("♡--------˖⁺‧₊˚♡˚₊‧⁺˖--------------˖⁺‧₊˚♡˚₊‧⁺˖--------------˖⁺‧₊˚♡˚₊‧⁺˖--------♡\n");
-    printf("|                                                                             |\n");
-    printf("|                                Excluir Produto                              |\n");
-    printf("|                                                                             |\n");
-    printf("|      Informe o ID do produto:                                               |\n");
     printf("|                                                                             |\n");
     printf("♡--------˖⁺‧₊˚♡˚₊‧⁺˖--------------˖⁺‧₊˚♡˚₊‧⁺˖--------------˖⁺‧₊˚♡˚₊‧⁺˖--------♡\n");
     printf("\n");
---
+++ produtos.h
@@ -0,0 +1,10 @@
+#ifndef PRODUTOS_H
+#define PRODUTOS_H
+
+void menu_produto(void);
+void cadastrar_produto(void);
+void exibir_produto(void);
+void alterar_produto(void);
+void excluir_produto(void);
+
+#endif
