92d4c3fe5084e1ce56aa1482272ff76c518860ba
wallisonvsdias <wallison.dias.711@ufrn.edu.br>
2025-09-15 13:10:03 -0300

    style: simplifica a moldura

--- main.c
+++ main.c
@@ -17,12 +17,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "clientes.h"
-#include "produtos.h"
-#include "vendas.h"
-#include "relatorios.h"
 #include "funcionarios.h"
 #include "quartos.h"
 #include "hospedagem.h"
+#include "produtos.h"
+#include "vendas.h"
+#include "relatorios.h"
 
 
 // Assinatura das funções
@@ -82,48 +82,48 @@ int main(void) {
 
 void menu_principal(void) {
     system("clear||cls");
-    printf("♡--------˖⁺‧₊˚♡˚₊‧⁺˖--------------˖⁺‧₊˚♡˚₊‧⁺˖--------------˖⁺‧₊˚♡˚₊‧⁺˖--------♡\n");
-    printf("|           Universidade Federal do Rio Grande do Norte - UFRN                |\n");
-    printf("|               Centro de Ensino Superior do Seridó - CERES                   |\n");
-    printf("|                     Disciplina DCT1106 -- Programação                       |\n");
-    printf("|                   Projeto Sistema de Gestão para Motéis                     |\n");
-    printf("|  Desenvolvido por @wallisonvsdias e @Isabellecavalcant -- desde Ago, 2025   |\n");
-    printf("|                                                                             |\n");
-    printf("|                                   SIG-Motel                                 |\n");
-    printf("♡--------˖⁺‧₊˚♡˚₊‧⁺˖--------------˖⁺‧₊˚♡˚₊‧⁺˖--------------˖⁺‧₊˚♡˚₊‧⁺˖--------♡\n");
-    printf("|                                                                             |\n");
-    printf("|           1 - Módulo Cliente                                                |\n");
-    printf("|           2 - Módulo Funcionário                                            |\n");
-    printf("|           3 - Módulo Quarto                                                 |\n");
-    printf("|           4 - Módulo Hospedagem                                             |\n");
-    printf("|           5 - Módulo Produto                                                |\n");
-    printf("|           6 - Módulo Venda                                                  |\n");
-    printf("|           7 - Módulo Relatórios                                             |\n");
-    printf("|           0 - Sair                                                          |\n");
-    printf("|                                                                             |\n");
-    printf("♡--------˖⁺‧₊˚♡˚₊‧⁺˖--------------˖⁺‧₊˚♡˚₊‧⁺˖--------------˖⁺‧₊˚♡˚₊‧⁺˖--------♡\n");
+    printf("♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡\n");
+    printf("♡           Universidade Federal do Rio Grande do Norte - UFRN                ♡\n");
+    printf("♡               Centro de Ensino Superior do Seridó - CERES                   ♡\n");
+    printf("♡                     Disciplina DCT1106 -- Programação                       ♡\n");
+    printf("♡                   Projeto Sistema de Gestão para Motéis                     ♡\n");
+    printf("♡  Desenvolvido por @wallisonvsdias e @Isabellecavalcant -- desde Ago, 2025   ♡\n");
+    printf("♡                                                                             ♡\n");
+    printf("♡                                   SIG-Motel                                 ♡\n");
+    printf("♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡\n");
+    printf("♡                                                                             ♡\n");
+    printf("♡           1 - Módulo Cliente                                                ♡\n");
+    printf("♡           2 - Módulo Funcionário                                            ♡\n");
+    printf("♡           3 - Módulo Quarto                                                 ♡\n");
+    printf("♡           4 - Módulo Hospedagem                                             ♡\n");
+    printf("♡           5 - Módulo Produto                                                ♡\n");
+    printf("♡           6 - Módulo Venda                                                  ♡\n");
+    printf("♡           7 - Módulo Relatórios                                             ♡\n");
+    printf("♡           0 - Sair                                                          ♡\n");
+    printf("♡                                                                             ♡\n");
+    printf("♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡\n");
     printf("\n");
 }
 
 void sobre(void) {
     system("clear||cls");
-    printf("♡--------˖⁺‧₊˚♡˚₊‧⁺˖--------------˖⁺‧₊˚♡˚₊‧⁺˖--------------˖⁺‧₊˚♡˚₊‧⁺˖--------♡\n");
-    printf("|           Universidade Federal do Rio Grande do Norte - UFRN                |\n");
-    printf("|               Centro de Ensino Superior do Seridó - CERES                   |\n");
-    printf("|                     Disciplina DCT1106 -- Programação                       |\n");
-    printf("|                   Projeto Sistema de Gestão para Motéis                     |\n");
-    printf("|  Desenvolvido por @wallisonvsdias e @Isabellecavalcant -- desde Ago, 2025   |\n");
-    printf("|                                                                             |\n");
-    printf("|                                   SIG-Motel                                 |\n");
-    printf("♡--------˖⁺‧₊˚♡˚₊‧⁺˖--------------˖⁺‧₊˚♡˚₊‧⁺˖--------------˖⁺‧₊˚♡˚₊‧⁺˖--------♡\n");
-    printf("|                                                                             |\n");
-    printf("| Esse projeto faz parte da disciplina Programação (DCT1106), ministrada pelo |\n");
-    printf("| professor Flavius Gorgônio. Nosso sistema auxilia os funcionários no        |\n");
-    printf("| gerenciamento de motéis, contando com módulos de cliente, funcionário,      |\n");
-    printf("| quarto, produto, hospedagem, venda, e os relatórios, incluíndo a listagem   |\n");
-    printf("| de todos os módulos anteriores.                                             |\n");
-    printf("|                                                                             |\n");
-    printf("♡--------˖⁺‧₊˚♡˚₊‧⁺˖--------------˖⁺‧₊˚♡˚₊‧⁺˖--------------˖⁺‧₊˚♡˚₊‧⁺˖--------♡\n");
+    printf("♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡\n");
+    printf("♡           Universidade Federal do Rio Grande do Norte - UFRN                ♡\n");
+    printf("♡               Centro de Ensino Superior do Seridó - CERES                   ♡\n");
+    printf("♡                     Disciplina DCT1106 -- Programação                       ♡\n");
+    printf("♡                   Projeto Sistema de Gestão para Motéis                     ♡\n");
+    printf("♡  Desenvolvido por @wallisonvsdias e @Isabellecavalcant -- desde Ago, 2025   ♡\n");
+    printf("♡                                                                             ♡\n");
+    printf("♡                                   SIG-Motel                                 ♡\n");
+    printf("♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡\n");
+    printf("♡                                                                             ♡\n");
+    printf("♡ Esse projeto faz parte da disciplina Programação (DCT1106), ministrada pelo ♡\n");
+    printf("♡ professor Flavius Gorgônio. Nosso sistema auxilia os funcionários no        ♡\n");
+    printf("♡ gerenciamento de motéis, contando com módulos de cliente, funcionário,      ♡\n");
+    printf("♡ quarto, produto, hospedagem, venda, e os relatórios, incluíndo a listagem   ♡\n");
+    printf("♡ de todos os módulos anteriores.                                             ♡\n");
+    printf("♡                                                                             ♡\n");
+    printf("♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡\n");
     printf("\n");
     printf("Pressione ENTER para voltar ao menu...");
     getchar();
@@ -132,33 +132,33 @@ void sobre(void) {
 
 void equipe(void) {
     system("clear||cls");
-    printf("♡--------˖⁺‧₊˚♡˚₊‧⁺˖--------------˖⁺‧₊˚♡˚₊‧⁺˖--------------˖⁺‧₊˚♡˚₊‧⁺˖--------♡\n");
-    printf("|           Universidade Federal do Rio Grande do Norte - UFRN                |\n");
-    printf("|               Centro de Ensino Superior do Seridó - CERES                   |\n");
-    printf("|                     Disciplina DCT1106 -- Programação                       |\n");
-    printf("|                   Projeto Sistema de Gestão para Motéis                     |\n");
-    printf("|  Desenvolvido por @wallisonvsdias e @Isabellecavalcant -- desde Ago, 2025   |\n");
-    printf("|                                                                             |\n");
-    printf("|                                   SIG-Motel                                 |\n");
-    printf("♡--------˖⁺‧₊˚♡˚₊‧⁺˖--------------˖⁺‧₊˚♡˚₊‧⁺˖--------------˖⁺‧₊˚♡˚₊‧⁺˖--------♡\n");
-    printf("|                                                                             |\n");
-    printf("| Esse projeto foi desenvolvido por:                                          |\n");
-    printf("|                                                                             |\n");
-    printf("| Wallison Dias                                                               |\n");
-    printf("| E-mail: wallison.dias.711@ufrn.edu.br                                       |\n");
-    printf("| GitHub: @wallisonvsdias                                                     |\n");
-    printf("|                                                                             |\n");
-    printf("| Isabelle Cavalcanti                                                         |\n");
-    printf("| E-mail: isabelle.silva.712@ufrn.edu.br                                      |\n");
-    printf("| GitHub: @Isabellecavalcant                                                  |\n");
-    printf("|                                                                             |\n");
-    printf("| Repositório: https://github.com/wallisonvsdias/sig-motel.git                |\n");
-    printf("|                                                                             |\n");
-    printf("| Sites usados para as artes em ASCII:                                        |\n");
-    printf("| https://emojicombos.com/heart                                               |\n");
-    printf("| https://www.asciiart.eu/text-to-ascii-art                                   |\n");
-    printf("|                                                                             |\n");
-    printf("♡--------˖⁺‧₊˚♡˚₊‧⁺˖--------------˖⁺‧₊˚♡˚₊‧⁺˖--------------˖⁺‧₊˚♡˚₊‧⁺˖--------♡\n");
+    printf("♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡\n");
+    printf("♡           Universidade Federal do Rio Grande do Norte - UFRN                ♡\n");
+    printf("♡               Centro de Ensino Superior do Seridó - CERES                   ♡\n");
+    printf("♡                     Disciplina DCT1106 -- Programação                       ♡\n");
+    printf("♡                   Projeto Sistema de Gestão para Motéis                     ♡\n");
+    printf("♡  Desenvolvido por @wallisonvsdias e @Isabellecavalcant -- desde Ago, 2025   ♡\n");
+    printf("♡                                                                             ♡\n");
+    printf("♡                                   SIG-Motel                                 ♡\n");
+    printf("♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡\n");
+    printf("♡                                                                             ♡\n");
+    printf("♡ Esse projeto foi desenvolvido por:                                          ♡\n");
+    printf("♡                                                                             ♡\n");
+    printf("♡ Wallison Dias                                                               ♡\n");
+    printf("♡ E-mail: wallison.dias.711@ufrn.edu.br                                       ♡\n");
+    printf("♡ GitHub: @wallisonvsdias                                                     ♡\n");
+    printf("♡                                                                             ♡\n");
+    printf("♡ Isabelle Cavalcanti                                                         ♡\n");
+    printf("♡ E-mail: isabelle.silva.712@ufrn.edu.br                                      ♡\n");
+    printf("♡ GitHub: @Isabellecavalcant                                                  ♡\n");
+    printf("♡                                                                             ♡\n");
+    printf("♡ Repositório: https://github.com/wallisonvsdias/sig-motel.git                ♡\n");
+    printf("♡                                                                             ♡\n");
+    printf("♡ Sites usados para as artes em ASCII:                                        ♡\n");
+    printf("♡ https://emojicombos.com/heart                                               ♡\n");
+    printf("♡ https://www.asciiart.eu/text-to-ascii-art                                   ♡\n");
+    printf("♡                                                                             ♡\n");
+    printf("♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡\n");
     printf("\n");
     printf("Pressione ENTER para voltar ao menu...");
     getchar();
@@ -168,25 +168,25 @@ void equipe(void) {
 void menu_hospedagem(void) {
     char op_hospedagem;
     system("clear||cls");
-    printf("♡--------˖⁺‧₊˚♡˚₊‧⁺˖--------------˖⁺‧₊˚♡˚₊‧⁺˖--------------˖⁺‧₊˚♡˚₊‧⁺˖--------♡\n");
-    printf("|           Universidade Federal do Rio Grande do Norte - UFRN                |\n");
-    printf("|               Centro de Ensino Superior do Seridó - CERES                   |\n");
-    printf("|                     Disciplina DCT1106 -- Programação                       |\n");
-    printf("|                   Projeto Sistema de Gestão para Motéis                     |\n");
-    printf("|  Desenvolvido por @wallisonvsdias e @Isabellecavalcant -- desde Ago, 2025   |\n");
-    printf("|                                                                             |\n");
-    printf("|                                   SIG-Motel                                 |\n");
-    printf("♡--------˖⁺‧₊˚♡˚₊‧⁺˖--------------˖⁺‧₊˚♡˚₊‧⁺˖--------------˖⁺‧₊˚♡˚₊‧⁺˖--------♡\n");
-    printf("|                                                                             |\n");
-    printf("|                               Módulo Hospedagem                             |\n");
-    printf("|                                                                             |\n");
-    printf("|      1 - Cadastrar Hospedagem                                               |\n");
-    printf("|      2 - Exibir Dados da Hospedagem                                         |\n");
-    printf("|      3 - Alterar Dados do Hospagem                                          |\n");
-    printf("|      4 - Cancelar Hospedagem                                                |\n");
-    printf("|      0 - Retornar ao Menu Principal                                         |\n");
-    printf("|                                                                             |\n");
-    printf("♡--------˖⁺‧₊˚♡˚₊‧⁺˖--------------˖⁺‧₊˚♡˚₊‧⁺˖--------------˖⁺‧₊˚♡˚₊‧⁺˖--------♡\n");
+    printf("♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡\n");
+    printf("♡           Universidade Federal do Rio Grande do Norte - UFRN                ♡\n");
+    printf("♡               Centro de Ensino Superior do Seridó - CERES                   ♡\n");
+    printf("♡                     Disciplina DCT1106 -- Programação                       ♡\n");
+    printf("♡                   Projeto Sistema de Gestão para Motéis                     ♡\n");
+    printf("♡  Desenvolvido por @wallisonvsdias e @Isabellecavalcant -- desde Ago, 2025   ♡\n");
+    printf("♡                                                                             ♡\n");
+    printf("♡                                   SIG-Motel                                 ♡\n");
+    printf("♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡\n");
+    printf("♡                                                                             ♡\n");
+    printf("♡                               Módulo Hospedagem                             ♡\n");
+    printf("♡                                                                             ♡\n");
+    printf("♡      1 - Cadastrar Hospedagem                                               ♡\n");
+    printf("♡      2 - Exibir Dados da Hospedagem                                         ♡\n");
+    printf("♡      3 - Alterar Dados do Hospagem                                          ♡\n");
+    printf("♡      4 - Cancelar Hospedagem                                                ♡\n");
+    printf("♡      0 - Retornar ao Menu Principal                                         ♡\n");
+    printf("♡                                                                             ♡\n");
+    printf("♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡\n");
     printf("\n");
     printf("Escolha sua opção: ");
     scanf("%c", &op_hospedagem);
