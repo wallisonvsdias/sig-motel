4a4c1e5874b5efd691f45000245f779aca60234d
Isabellecavalcant <isabelle.silva.712@ufrn.edu.br>
2025-09-15 11:34:33 -0300

    feat: Modulariza o menu funcionários

---
+++ funcionarios.h
@@ -0,0 +1,10 @@
+#ifndef FUNCIONARIOS_H
+#define FUNCIONARIOS_H
+
+void menu_funcionario(void);
+void cadastrar_funcionario(void);
+void exibir_funcionario(void);
+void alterar_funcionario(void);
+void excluir_funcionario(void);
+
+#endif
--- main.c
+++ main.c
@@ -20,18 +20,13 @@
 #include "produtos.h"
 #include "vendas.h"
 #include "relatorios.h"
+#include "funcionarios.h"
 
 
 // Assinatura das funções
 void menu_principal(void);
 void sobre(void);
 void equipe(void);
-
-void menu_funcionario(void);
-void cadastrar_funcionario(void);
-void exibir_funcionario(void);
-void alterar_funcionario(void);
-void excluir_funcionario(void);
 
 void menu_quarto(void);
 void cadastrar_quarto(void);
@@ -177,154 +172,6 @@ void equipe(void) {
     printf("|                                                                             |\n");
     printf("♡--------˖⁺‧₊˚♡˚₊‧⁺˖--------------˖⁺‧₊˚♡˚₊‧⁺˖--------------˖⁺‧₊˚♡˚₊‧⁺˖--------♡\n");
     printf("\n");
-    printf("Pressione ENTER para voltar ao menu...");
-    getchar();
-}
-
-
-void menu_funcionario(void) {
-    char op_funcionario;
-    system("clear||cls");
-    printf("♡--------˖⁺‧₊˚♡˚₊‧⁺˖--------------˖⁺‧₊˚♡˚₊‧⁺˖--------------˖⁺‧₊˚♡˚₊‧⁺˖--------♡\n");
-    printf("|           Universidade Federal do Rio Grande do Norte - UFRN                |\n");
-    printf("|               Centro de Ensino Superior do Seridó - CERES                   |\n");
-    printf("|                     Disciplina DCT1106 -- Programação                       |\n");
-    printf("|                   Projeto Sistema de Gestão para Motéis                     |\n");
-    printf("|  Desenvolvido por @wallisonvsdias e @Isabellecavalcant -- desde Ago, 2025   |\n");
-    printf("|                                                                             |\n");
-    printf("|                                   SIG-Motel                                 |\n");
-    printf("♡--------˖⁺‧₊˚♡˚₊‧⁺˖--------------˖⁺‧₊˚♡˚₊‧⁺˖--------------˖⁺‧₊˚♡˚₊‧⁺˖--------♡\n");
-    printf("|                                                                             |\n");
-    printf("|                             Módulo Funcionário                              |\n");
-    printf("|                                                                             |\n");
-    printf("|      1 - Cadastrar Funcioário(a)                                            |\n");
-    printf("|      2 - Exibir Dados do Funcionário(a)                                     |\n");
-    printf("|      3 - Alterar Dados do FUncionário(a)                                    |\n");
-    printf("|      4 - Excluir Funcionário                                                |\n");
-    printf("|      0 - Retornar ao Menu Principal                                         |\n");
-    printf("|                                                                             |\n");
-    printf("♡--------˖⁺‧₊˚♡˚₊‧⁺˖--------------˖⁺‧₊˚♡˚₊‧⁺˖--------------˖⁺‧₊˚♡˚₊‧⁺˖--------♡\n");
-    printf("\n");
-    printf("Escolha sua opção: ");
-    scanf("%c", &op_funcionario);
-    getchar();
-    
-    switch(op_funcionario) {
-        case '1':
-            cadastrar_funcionario();
-            break;
-        case '2':
-            exibir_funcionario();
-            break;
-        case '3':
-            alterar_funcionario();
-            break;
-        case '4':
-            excluir_funcionario();
-            break;
-        default:
-            printf("Por favor, digite uma opção válida");
-            break;
-    }
-}
-
-
-void cadastrar_funcionario(void) {
-    system("clear||cls");
-    printf("♡--------˖⁺‧₊˚♡˚₊‧⁺˖--------------˖⁺‧₊˚♡˚₊‧⁺˖--------------˖⁺‧₊˚♡˚₊‧⁺˖--------♡\n");
-    printf("|           Universidade Federal do Rio Grande do Norte - UFRN                |\n");
-    printf("|               Centro de Ensino Superior do Seridó - CERES                   |\n");
-    printf("|                     Disciplina DCT1106 -- Programação                       |\n");
-    printf("|                   Projeto Sistema de Gestão para Motéis                     |\n");
-    printf("|  Desenvolvido por @wallisonvsdias e @Isabellecavalcant -- desde Ago, 2025   |\n");
-    printf("|                                                                             |\n");
-    printf("|                                   SIG-Motel                                 |\n");
-    printf("♡--------˖⁺‧₊˚♡˚₊‧⁺˖--------------˖⁺‧₊˚♡˚₊‧⁺˖--------------˖⁺‧₊˚♡˚₊‧⁺˖--------♡\n");
-    printf("|                                                                             |\n");
-    printf("|                           Cadastrar Funcionário(a)                          |\n");
-    printf("|                                                                             |\n");
-    printf("|      Nome:                                                                  |\n");
-    printf("|      Cpf:                                                                   |\n");
-    printf("|      Telefone:                                                              |\n");
-    printf("|      E-mail:                                                                |\n");
-    printf("|      Cargo:                                                                 |\n");
-    printf("|      Salário:                                                               |\n");
-    printf("|                                                                             |\n");
-    printf("♡--------˖⁺‧₊˚♡˚₊‧⁺˖--------------˖⁺‧₊˚♡˚₊‧⁺˖--------------˖⁺‧₊˚♡˚₊‧⁺˖--------♡\n");
-    printf("\n");
-    getchar();
-    printf("Pressione ENTER para voltar ao menu...");
-    getchar();
-}
-
-
-void exibir_funcionario(void) {
-    system("clear||cls");
-    printf("♡--------˖⁺‧₊˚♡˚₊‧⁺˖--------------˖⁺‧₊˚♡˚₊‧⁺˖--------------˖⁺‧₊˚♡˚₊‧⁺˖--------♡\n");
-    printf("|           Universidade Federal do Rio Grande do Norte - UFRN                |\n");
-    printf("|               Centro de Ensino Superior do Seridó - CERES                   |\n");
-    printf("|                     Disciplina DCT1106 -- Programação                       |\n");
-    printf("|                   Projeto Sistema de Gestão para Motéis                     |\n");
-    printf("|  Desenvolvido por @wallisonvsdias e @Isabellecavalcant -- desde Ago, 2025   |\n");
-    printf("|                                                                             |\n");
-    printf("|                                   SIG-Motel                                 |\n");
-    printf("♡--------˖⁺‧₊˚♡˚₊‧⁺˖--------------˖⁺‧₊˚♡˚₊‧⁺˖--------------˖⁺‧₊˚♡˚₊‧⁺˖--------♡\n");
-    printf("|                                                                             |\n");
-    printf("|                        Exibir Dados do Funcionário(a)                       |\n");
-    printf("|                                                                             |\n");
-    printf("|     Informe o cpf do funcionário:                                           |\n");
-    printf("|                                                                             |\n");
-    printf("♡--------˖⁺‧₊˚♡˚₊‧⁺˖--------------˖⁺‧₊˚♡˚₊‧⁺˖--------------˖⁺‧₊˚♡˚₊‧⁺˖--------♡\n");
-    printf("\n");
-    getchar();
-    printf("Pressione ENTER para voltar ao menu...");
-    getchar();
-}
-
-
-void alterar_funcionario(void) {
-    system("clear||cls");
-    printf("♡--------˖⁺‧₊˚♡˚₊‧⁺˖--------------˖⁺‧₊˚♡˚₊‧⁺˖--------------˖⁺‧₊˚♡˚₊‧⁺˖--------♡\n");
-    printf("|           Universidade Federal do Rio Grande do Norte - UFRN                |\n");
-    printf("|               Centro de Ensino Superior do Seridó - CERES                   |\n");
-    printf("|                     Disciplina DCT1106 -- Programação                       |\n");
-    printf("|                   Projeto Sistema de Gestão para Motéis                     |\n");
-    printf("|  Desenvolvido por @wallisonvsdias e @Isabellecavalcant -- desde Ago, 2025   |\n");
-    printf("|                                                                             |\n");
-    printf("|                                   SIG-Motel                                 |\n");
-    printf("♡--------˖⁺‧₊˚♡˚₊‧⁺˖--------------˖⁺‧₊˚♡˚₊‧⁺˖--------------˖⁺‧₊˚♡˚₊‧⁺˖--------♡\n");
-    printf("|                                                                             |\n");
-    printf("|                       Alterar Dados do Funcionário(a)                       |\n");
-    printf("|                                                                             |\n");
-    printf("|     Informe o cpf do funcionário:                                           |\n");
-    printf("|                                                                             |\n");
-    printf("♡--------˖⁺‧₊˚♡˚₊‧⁺˖--------------˖⁺‧₊˚♡˚₊‧⁺˖--------------˖⁺‧₊˚♡˚₊‧⁺˖--------♡\n");
-    printf("\n");
-    getchar();
-    printf("Pressione ENTER para voltar ao menu...");
-    getchar();
-}
-
-
-void excluir_funcionario(void){
-    system("clear||cls");
-    printf("♡--------˖⁺‧₊˚♡˚₊‧⁺˖--------------˖⁺‧₊˚♡˚₊‧⁺˖--------------˖⁺‧₊˚♡˚₊‧⁺˖--------♡\n");
-    printf("|           Universidade Federal do Rio Grande do Norte - UFRN                |\n");
-    printf("|               Centro de Ensino Superior do Seridó - CERES                   |\n");
-    printf("|                     Disciplina DCT1106 -- Programação                       |\n");
-    printf("|                   Projeto Sistema de Gestão para Motéis                     |\n");
-    printf("|  Desenvolvido por @wallisonvsdias e @Isabellecavalcant -- desde Ago, 2025   |\n");
-    printf("|                                                                             |\n");
-    printf("|                                   SIG-Motel                                 |\n");
-    printf("♡--------˖⁺‧₊˚♡˚₊‧⁺˖--------------˖⁺‧₊˚♡˚₊‧⁺˖--------------˖⁺‧₊˚♡˚₊‧⁺˖--------♡\n");
-    printf("|                                                                             |\n");
-    printf("|                             Excluir Funcionário(a)                             |\n");
-    printf("|                                                                             |\n");
-    printf("|      Informe o cpf do funcionário:                                          |\n");
-    printf("|                                                                             |\n");
-    printf("♡--------˖⁺‧₊˚♡˚₊‧⁺˖--------------˖⁺‧₊˚♡˚₊‧⁺˖--------------˖⁺‧₊˚♡˚₊‧⁺˖--------♡\n");
-    printf("\n");
-    getchar();
     printf("Pressione ENTER para voltar ao menu...");
     getchar();
 }
