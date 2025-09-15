c9594a597e92e8debc3184675c35bc6f9c81d715
Isabellecavalcant <isabelle.silva.712@ufrn.edu.br>
2025-09-15 11:54:39 -0300

    feat: Modulariza menu quartos

---
+++ quartos.h
@@ -0,0 +1,10 @@
+#ifndef QUARTOS_H
+#define QUARTOS_H
+
+void menu_quarto(void);
+void cadastrar_quarto(void);
+void exibir_quarto(void);
+void alterar_quarto(void);
+void excluir_quarto(void);
+
+#endif
