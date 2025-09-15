5a43fcd245572690c35a809d721992373f8b14b3
wallisonvsdias <wallison.dias.711@ufrn.edu.br>
2025-09-15 16:44:20 -0300

    fix: coloca finalização de volta na main

--- main.c
+++ main.c
@@ -143,3 +143,22 @@ void equipe(void) {
     printf("Pressione ENTER para voltar ao menu...");
     getchar();
 }
+
+
+void finalizacao(void) {
+    system("clear||cls");
+    printf("♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡\n");
+    printf("♡                            SISTEMA ENCERRADO                                ♡\n");
+    printf("♡                                                                             ♡\n");
+    printf("♡                 Obrigado por utilizar o SIG-Motel!                          ♡\n");
+    printf("♡                                                                             ♡\n");
+    printf("♡ Este sistema auxilia no gerenciamento de clientes, funcionários, quartos,   ♡\n");
+    printf("♡ produtos, hospedagens, vendas e relatórios, garantindo organização e        ♡\n");
+    printf("♡ eficiência na administração do motéis.                                      ♡\n");
+    printf("♡                                                                             ♡\n");
+    printf("♡                             Até a próxima!                                  ♡\n");
+    printf("♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡\n");
+    printf("\n");
+    printf("Pressione ENTER para sair...");
+    getchar();
+}
