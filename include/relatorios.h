1dd7daf3fed1e27e7c6ab8bc51bbc376997fabc4
Pedro William <pedro.marcal.079@ufrn.edu.br>
2025-11-09 16:01:11 -0300

    fix: navegação no menu de relatórios e lista geral de hospedagens

--- include/relatorios.h
+++ include/relatorios.h
@@ -6,7 +6,7 @@ void menu_relatorio(void);
 void lista_geral_clientes(void);
 void lista_geral_funcionarios(void);
 void lista_geral_quartos(void);
-void lista_geral_quartos(void);
+void lista_geral_hospedagens(void);
 void lista_geral_produtos(void);
 void lista_geral_vendas(void);
 
