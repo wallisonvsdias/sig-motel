2efa7d248f2ec275115afe873980b4b62041b9d3
Pedro William <pedro.marcal.079@ufrn.edu.br>
2025-11-16 14:54:40 -0300

    style: tira relatorios nao implementados

--- src/relatorios.c
+++ src/relatorios.c
@@ -24,16 +24,6 @@ void menu_relatorio(void) {
         printf("♡      4  - Lista geral de hospedagens                                        ♡\n");
         printf("♡      5  - Lista geral de produtos                                           ♡\n");
         printf("♡      6  - Lista geral de vendas                                             ♡\n");
-        printf("♡      7  - Lista de hospedagens por cliente                                  ♡\n");
-        printf("♡      8  - Lista de hospedagens por funcionário                              ♡\n");
-        printf("♡      9  - Lista de hospedagens por quarto                                   ♡\n");
-        printf("♡     10  - Lista de hospedagens por período                                  ♡\n");
-        printf("♡     11  - Lista de vendas por cliente                                       ♡\n");
-        printf("♡     12  - Lista de vendas por funcionário                                   ♡\n");
-        printf("♡     13  - Lista de vendas por produto                                       ♡\n");
-        printf("♡     14  - Lista de vendas por período                                       ♡\n");
-        printf("♡     15  - Lista de gastos totais por cliente                                ♡\n");
-        printf("♡     16  - Lista de lucros totais                                            ♡\n");
         printf("♡      0  - Retornar ao Menu Principal                                        ♡\n");
         printf("♡                                                                             ♡\n");
         printf("♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡\n");
